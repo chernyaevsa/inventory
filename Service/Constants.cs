1a0ccf717d3d0b1eb135cde9754a56243df0e79a
Chernyaev Sergei <csa@czsa.ru>
2024-08-12 10:35:57 +0500

    add template of service

---
+++ Service/Constants.cs
@@ -0,0 +1,15 @@
+namespace Service
+{
+   public static class Constants
+    {
+        public const string AuthDBDatabaseName = "SERVICE_DB_DATABASE";
+        public const string SERVICEDBUserName = "SERVICE_DB_USER";
+        public const string SERVICEDBPasswordName = "SERVICE_DB_PASSWORD";
+        public const string SERVICEDBPortName = "SERVICE_DB_PORT";
+        public const string ApiKeyName = "SERVICE_API_KEY";
+        public const string SERVICEDBServerName = "SERVICE_DB_SERVER_NAME";
+
+        public const string ApiKeyErrorMessage = "ApiKey is not valid";
+    } 
+}
+
